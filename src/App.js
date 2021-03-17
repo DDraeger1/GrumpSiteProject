1154e51b42a30edafc4e795992471e6171e67359
DDraeger1 <74009750+DDraeger1@users.noreply.github.com>
2021-03-17 13:13:14 +0100

    First Update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./css/grumpBackground.css"
+import './App.css';
+
+function App() {
+  return (
+<div class="bg-gradient">
+  <div class="bg-dots">
+  </div>
+</div>
+  );
+}
+
+export default App;
